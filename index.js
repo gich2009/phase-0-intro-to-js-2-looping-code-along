38b887479f1beac55a9096d76751468dcf360410
gich2009 <waneneamos@gmail.com>
2023-03-18 00:46:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+/****************IMPLEMENTATION NUMBER 1 OF writeCards()***************/
+function writeCards(nameArray){
+  let cardArray = [...nameArray];
+
+  for (let i = 0 ; i < cardArray.length ; ++i){
+    cardArray[i] = `Thank you, ${cardArray[i]}, for the wonderful surprise gift!`;
+  }
+
+  return cardArray;
+  
+}
+
+/****************IMPLEMENTATION NUMBER 2 OF writeCards()***************/
+// function writeCards(nameArray){
+//   let cardArray = [];
+
+//   for (let i = 0 ; i < nameArray.length ; ++i){
+//     cardArray.push(`Thank you, ${nameArray[i]}, for the wonderful surprise gift!`);
+//   }
+
+//   return cardArray;
+// }
+
+
+function countDown(startingNumber){
+
+  for (let i = startingNumber ; i >= 0 ; --i){
+    console.log(i);
+  }
+
+}
